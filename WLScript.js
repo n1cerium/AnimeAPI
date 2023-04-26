799bc4950a86df9e2fedada95000d42d85b9aae5
Gevin Sarmiento <114702605+n1cerium@users.noreply.github.com>
2023-04-26 15:55:29 -0700

    Fix Issue

--- WLScript.js
+++ WLScript.js
@@ -1,6 +1,6 @@
 const {physics, styler} = popmotion;
 
-let currentList = [];
+let allList;
 
 function crtElem(tagName, attributeType, className) {
     const node = document.createElement(tagName);
@@ -26,46 +26,55 @@ function shortenWord(desc) {
 }
 function display() {
     const res = document.getElementById("result");
-    currentList = JSON.parse(localStorage.getItem("Lists"));
+    let currentList = JSON.parse(localStorage.getItem("Lists"));
+    allList = currentList;
     for(let i = 0; i < currentList.length; i++) {
+        current =  currentList[i][0];
         const newDiv = crtElem("div", "class", "Item");
         console.log(currentList[i]);
         const dImg = crtElem("div", "class", "ImageDiv");
-        const AnimImg = crtElem("img", "src", currentList[i][0].image);
+        const AnimImg = crtElem("img", "src", current.image);
         dImg.appendChild(AnimImg);
 
         const dTitle = crtElem("div", "class", "TitleDiv");
-        const pTitle = crtElemText("h3", "id", "Title", currentList[i][0].title);
+        const pTitle = crtElemText("h3", "id", "Title", current.title);
         dTitle.appendChild(pTitle);
 
         const DInfo1 = crtElem("div", "class", "Info1");
-        const pGenres = crtElemText("p", "class", "Genre", "Genre: " + currentList[i][0].genre.join(", "));
+        let pGenres;
+        if(current.genre.length == 1) {
+            pGenres = crtElemText("p", "class", "Genre", "Genre: " + current.genre[0]);
+
+        } else {
+            pGenres = crtElemText("p", "class", "Genre", "Genre: " + current.genre.join(", "));
+
+        }
         
         let pStatus= crtElemText("p", "class", "Status", "Status: Complete");
-        if(currentList[i][0].status == "Currently Airing") {
+        if(current.status == "Currently Airing") {
             pStatus = crtElemText("p", "class", "Status", "Status: OnGoing");
-        } else if (currentList[i][0].status == "Not yet aired") {
+        } else if (current.status == "Not yet aired") {
             pStatus = crtElemText("p", "class", "Status", "Status: Not yet aired");
         }
-        const pSeason = crtElemText("p", "class", "Season", "Season: " + currentList[i][0].season);
+        const pSeason = crtElemText("p", "class", "Season", "Season: " + current.season);
         DInfo1.appendChild(pGenres);
         DInfo1.appendChild(pStatus);
         DInfo1.appendChild(pSeason);
 
         const DInfo2 = crtElem("div", "class", "Info2");
 
-        let episodes = currentList[i][0].episodes;
-        if(currentList[i][0].episodes == null) {
+        let episodes = current.episodes;
+        if(current.episodes == null) {
             episodes = "Unknown";
         } 
-        const pEpisodes = crtElemText("p", "class", "Episodes", "Total Episode(s): " + currentList[i][0].episodes);
-        const pDuration = crtElemText("p", "class", "Duration", "Duration: " + currentList[i][0].duration);
+        const pEpisodes = crtElemText("p", "class", "Episodes", "Total Episode(s): " + current.episodes);
+        const pDuration = crtElemText("p", "class", "Duration", "Duration: " + current.duration);
         DInfo2.appendChild(pEpisodes);
         DInfo2.appendChild(pDuration);
 
         const dDesc = crtElem("div", "class", "DescDiv");
         const hDesc = crtElemText("h3", "class", "Desc", "Description: ");
-        const pDescription = crtElemText("p", "class", "Synopsis", shortenWord(currentList[i][0].description));
+        const pDescription = crtElemText("p", "class", "Synopsis", shortenWord(current.description));
         dDesc.appendChild(hDesc);
         dDesc.appendChild(pDescription);
 
@@ -115,9 +124,9 @@ function start() {
                 
             }).start(styleDiv.set);
 
-            currentList.splice(i,1);
-            console.log(currentList);
-            localStorage.setItem("Lists", JSON.stringify(currentList));
+            allList.splice(i,1);
+            console.log(allList);
+            localStorage.setItem("Lists", JSON.stringify(allList));
             setTimeout(() => {
                 res.removeChild(allItem[i]);
               }, "2000");
